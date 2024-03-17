474592af9a5e33ecfac4020023e031519c3937c2
Bret Petersen <bretpetersen82@gmail.com>
2024-03-16 21:42:58 -0500

    Initial commit

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,39 @@
+import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+
+const links = [
+  { name: 'Home', href: '/' },
+  { name: 'Journals', href: '/journal' },
+  { name: 'History', href: '/history' },
+]
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <div className="h-screen w-screen relative">
+      <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
+        <div className="px-4 my-4">
+          <span className="text-3xl">MOOD</span>
+        </div>
+        <div>
+          <ul className="px-4">
+            {links.map((link) => (
+              <li key={link.name} className="text-xl my-4">
+                <Link href={link.href}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </aside>
+      <div className="ml-[200px] h-full">
+        <header className="h-[60px] border-b border-black/10">
+          <div className="h-full w-full px-6 flex items-center justify-end">
+            <UserButton />
+          </div>
+        </header>
+        <div className="h-[calc(100vh-60px)]">{children}</div>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardLayout
---
+++ components/Editor.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import { updatedEntry } from '@/utils/api'
+import { useState } from 'react'
+import { useAutosave } from 'react-autosave'
+import Spinner from './Spinner'
+
+const Editor = ({ entry }) => {
+  const [value, setValue] = useState(entry.content)
+  const [isSaving, setIsSaving] = useState(false)
+  const [analysis, setAnalysis] = useState(entry.analysis)
+
+  const { mood, summary, color, negative, subject } = analysis
+  const analysisData = [
+    {
+      name: 'Summary',
+      value: summary,
+    },
+    {
+      name: 'Subject',
+      value: subject,
+    },
+    {
+      name: 'Mood',
+      value: mood,
+    },
+    {
+      name: 'Negative',
+      value: negative ? 'True' : 'False',
+    },
+  ]
+
+  useAutosave({
+    data: value,
+    onSave: async (_value) => {
+      setIsSaving(true)
+      const data = await updatedEntry(entry.id, _value)
+      setAnalysis(data.analysis)
+      setIsSaving(false)
+    },
+  })
+
+  return (
+    <div className="w-full h-full grid grid-cols-3 gap-0 relative">
+      <div className="absolute left-0 top-0 p-2">
+        {isSaving ? (
+          <Spinner />
+        ) : (
+          <div className="w-[16px] h-[16px] rounded-full bg-green-500"></div>
+        )}
+      </div>
+      <div className="col-span-2">
+        <textarea
+          className="w-full h-full p-8 text-xl outline-none"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+
+      <div className="border-l border-black/10">
+        <div className="px-6 py-10" style={{ backgroundColor: color }}>
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <div>
+          <ul>
+            {analysisData.map((item) => (
+              <li
+                key={item.name}
+                className="px-2 py-4 flex items-center justify-between border-b border-t border-black/10"
+              >
+                <span className="text-lg font-semibold">{item.name}</span>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Editor
