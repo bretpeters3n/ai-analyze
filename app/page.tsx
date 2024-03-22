9340a39f869def99ee83380c39f5c84e78d618e8
Bret Petersen <bretpetersen@gmail.com>
2024-03-22 13:40:55 -0500

    Homepage structure updates

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,18 @@ export default async function Home() {
   let href = userId ? '/journal' : '/new-user'
 
   return (
-    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white p-5">
       <div className="w-full max-w-[600px] mx-auto">
-        <h1 className="text-6xl mb-4">Best AI Journal app, period.</h1>
-        <p className="text-2xl text-white/60 mb-4">
+        <h1 className="text-5xl sm:text-6xl mb-4">
+          Best AI Journal app, period.
+        </h1>
+        <p className="text-1xl sm:text-2xl text-white/60 mb-4">
           This is the best app for tracking our mood. All you have to do is be
           honest.
         </p>
         <div>
           <Link href={href}>
-            <button className="bg-blue-600 px-5 py-3 rounded-lg text-xl">
+            <button className="bg-blue-600 px-4 px-5 py-3 rounded-lg text-lg sm:text-xl">
               Get Started
             </button>
           </Link>
